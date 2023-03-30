65596f5e58a3c3d3410122b27d2b48c5c0fe481b
Nicolas Mailloux <nicolecrivain@gmail.com>
2023-03-30 19:30:44 -0400

    Fix misleading echo

--- release.sh
+++ release.sh
@@ -13,5 +13,5 @@ rm -f ../overlaymount-rootfs.squashfs
 mksquashfs . ../overlaymount-rootfs.squashfs -b 1048576 -comp xz -Xdict-size 100% -always-use-fragments -all-root -e .git -e .gitignore -e .vscode -e release.sh
 rm ./.commit
 find . -type d ! -path "*.git*" -empty -exec touch '{}'/.gitkeep \;
-echo "Overlaymount gui-rootfs filesystem has been compressed."
+echo "overlaymount-rootfs filesystem has been compressed."
 
